f7efa4a3b9008a2c87200c9db472766a91da2e3d
achmegantara <achmegantara@gmail.com>
2021-02-21 23:46:35 +0700

    inisialisasi ulang project react, create new project react

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import "assets/scss/style.css";
+import "assets/scss/style.scss";
 
 function App() {
   return (
-    <div className="App">
+  <div className="App">
       Hai dunia!
     </div>
   );
