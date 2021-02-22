919cbef8098a5891081b9cbaefc5d0fa7ef4ee79
achmegantara <achmegantara@gmail.com>
2021-02-22 21:03:49 +0700

    update variable icon

---
+++ front-end/src/parts/iconText.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from 'element/Button';
+
+export default function iconText() {
+    return (
+    <Button className="brand-text-icon" href="" type="link">
+        Stay=<span className="text-grey-900">cation.</span>
+    </Button>
+    )
+}
