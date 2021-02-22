a8aad43580870404d932bdbe408ef053d33fa6ca
achmegantara <achmegantara@gmail.com>
2021-02-22 20:21:49 +0700

    add button component and testing component

--- front-end/src/elements/Button/index.js
+++ front-end/src/elements/Button/index.js
@@ -1,7 +1,6 @@
-import React from 'react'
-
-import { link } from 'react-router-dom'
-import propTypes from 'prop-types'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import propTypes from 'prop-types';
 
 export default function Button(props) {
     const className = [props.className]
@@ -15,22 +14,52 @@ export default function Button(props) {
         if(props.onClick) props.onClick()
     }
 
+    if(props.isDisabled || props.isLoading){
+        if(props.isDisabled) className.push("disabled");
+        return (
+            <span className={className.join(" ")} style={props.style}>
+                {props.isLoading} ? (
+                    <>
+                    <span className="spinner-border spinner-border-sm mx-5"></span>
+                    <span className="sr-only">Loading...</span>
+                    </>
+                ) : (
+                    {
+                        props.children
+                    }
+                )
+            </span>
+        );
+    }
+
     if(props.type === "link") {
         if(props.isExternal){
         return (
-            <a href={props.href} className={className.join(" ")} style={props.style} target={props.target==="_blank" ? "_blank":undefined} rel={props.target ==="_blank" ? "noopener noreferrer": undefined}>{props.children}</a>
+            <a 
+            href={props.href} 
+            className={className.join(" ")} 
+            style={props.style} 
+            target={props.target === "_blank" ? "_blank" : undefined} 
+            rel={props.target === "_blank" ? "noopener noreferrer" : undefined}>
+            {props.children}
+            </a>
         )
         } else {
             return (
-                <link to={props.href} className={className.join(" ")} style={props.style} onClick={onClick}>
-                    
+                <Link to={props.href} className={className.join(" ")} style={props.style} onClick={onClick}>
                 {props.children}
-                </link>
+                </Link>
             )
         }
     }
 
-    return <div></div>;
+    return <button
+    className={className.join(" ")}
+    style={props.style}
+    onClick={onClick}
+    >
+    {props.children}
+    </button>;
 }
 
 Button.propTypes = {
