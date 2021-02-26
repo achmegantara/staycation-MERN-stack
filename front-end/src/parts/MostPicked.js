e4f42ec5f9484c83a246c9ff545d25edbe5d642d
achmegantara <achmegantara@gmail.com>
2021-02-26 23:19:10 +0700

    fixed line

--- front-end/src/parts/MostPicked.js
+++ front-end/src/parts/MostPicked.js
@@ -6,31 +6,23 @@ export default function MostPicked(props) {
             <h4 className="mb-3">Most Picked</h4>
             <div className="row">
                 <div className="col-4">
-                    <div className="card h-100"></div>
+                    <div className="card h-100"> card 1</div>
                 </div>
                 <div className="col-8">
                     <div className="row mb-3">
                         <div className="col-6">
-                            <div className="card">
-                                Card 2
-                            </div>
+                            <div className="card">Card 2</div>
                         </div>
                         <div className="col-6">
-                        <div className="card">
-                                Card 2
-                            </div>
+                        <div className="card"> Card 2 </div>
                         </div>
                     </div>
                     <div className="row">
                     <div className="col-6">
-                    <div className="card">
-                                Card 3
-                            </div>
+                    <div className="card"> Card 3 </div>
                     </div>
                     <div className="col-6">
-                    <div className="card">
-                                Card 4
-                            </div>
+                    <div className="card"> Card 4 </div>
                     </div>
                     </div>
                 </div>
