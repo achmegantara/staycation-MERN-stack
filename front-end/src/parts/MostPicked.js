f061f2df5dfba0c557636da93d8bddb043c4e50a
achmegantara <achmegantara@gmail.com>
2021-03-01 20:16:46 +0700

    markup mostpicked complete

--- front-end/src/parts/MostPicked.js
+++ front-end/src/parts/MostPicked.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from 'elements/Button';
 
 export default function MostPicked(props) {
     return (
@@ -9,7 +10,23 @@ export default function MostPicked(props) {
                     props.data.map((item, index) => {
                     return (
                     <div className={`item column-4${index === 0 ? " row-2" : " row-1"}`}>
-                    <div className="card">Card {index + 1}</div>
+                    <div className="card card-featured">
+                        <div className="tag">
+                        ${item.price}
+                        <span className="font-weight-light">per {item.unit}</span>
+                        </div>
+                        <figure className="img-wrapper">
+                            <img src={item.imageUrl} alt={item.name} className="img-cover"></img>
+                        </figure>
+                        <div className="metwa-wrapper">
+                            <Button type="link" className="streched-link d-block text-white" href={`/properties/${item._id}`}>
+                                <h5>{item.name}</h5>
+                            </Button>
+                            <span>
+                                {item.city}, {item.country}
+                            </span>
+                        </div>
+                    </div>
                 </div>
                     )
                     })
