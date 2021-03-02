51df2cc4267ec47849a33aae5131ed9090d92c1b
achmegantara <achmegantara@gmail.com>
2021-03-02 22:14:20 +0700

    add component in category section

--- front-end/src/parts/Categories.js
+++ front-end/src/parts/Categories.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from 'elements/Button';
 
 export default function Categories({ data }) {
 
@@ -24,6 +25,19 @@ return data.map((category, index1) => {
                             <span className="font-weight-light">Choice</span>
                             </div>
                         )}
+                        <figure className="img-wrapper" style={{height:180}}>
+                            <img src={item.imageUrl} alt={item.name} className="img-cover"></img>
+                        </figure>
+                        <div className="meta-wrapper">
+                            <Button href={`/properties/${item._id}`} className="stretched-link d-block text-gray-800">
+                                <h5 className="h4">
+                                    {item.name}
+                                </h5>
+                            </Button>
+                            <span className="text-gray-500">
+                                {item.city}, {item.country}
+                            </span>
+                        </div>
                         </div>
                         </div>
                     );
