46d25960d0ee2b625b3a2b430ee5446659f6e194
achmegantara <achmegantara@gmail.com>
2021-03-04 00:06:14 +0700

    fix the code for testimony

--- front-end/src/elements/Star/index.js
+++ front-end/src/elements/Star/index.js
@@ -2,16 +2,20 @@ import React from "react";
 import propTypes from "prop-types";
 import "./index.scss";
 
-export default function Star(className, value, height, width, spacing) {
+export default function Star({className, value, height, width, spacing}) {
     const decimals = Number(value) % 1;
     const star = [];
     let leftPos = 0;
-    for(let index = 0; index < 5 && index < value - decimals; index++){
+    for (let index = 0; index < 5 && index < value - decimals; index++){
         leftPos = leftPos + width;
         star.push(
             <div className="star" 
             key={`star-${index}`} 
-            style={{left: index * width, height: height, width: width, marginRight: spacing}}>
+            style={{left: index * width,
+            height: height,
+            width: width,
+            marginRight: spacing
+            }}>
             </div>
         );
         }
@@ -25,18 +29,22 @@ export default function Star(className, value, height, width, spacing) {
     
 
     const starPlaceHolder = [];
-    for(let index =0; index < 5; index++){
+    for (let index = 0; index < 5; index++){
         starPlaceHolder.push(
             <div className="star placeholder" 
             key={`starPlaceholder-${index}`} 
-            style={{left: index * width, width: width, marginRight: spacing}}>
+            style={{left: index * width, 
+            height: height,
+            width: width, 
+            marginRight: spacing}}>
             </div>
         );
     }
 
     return (
         <>
-        <div className={["stars", className].join(" ")} style={{height: height}}>
+        <div className={["stars", className].join(" ")} 
+        style={{height: height}}>
         {starPlaceHolder}
         {star}
         </div>
@@ -50,4 +58,4 @@ Star.propTypes = {
     width: propTypes.number,
     height: propTypes.number,
     spacing: propTypes.number 
-}
+};
