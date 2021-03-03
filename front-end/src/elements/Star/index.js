42de2f3c308b02626e8ea676b465c42c726a8f70
achmegantara <achmegantara@gmail.com>
2021-03-03 22:59:57 +0700

    create smart component for star and create scss for color variables

---
+++ front-end/src/elements/Star/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import propTypes from 'prop-types';
+
+export default function Star(value, height, width, spacing) {
+   
+    const decimals = Number(value) % 1;
+    const star = [];
+    let leftPos = 0;
+    for(let index =0; index < 5 && index < value - decimals; index++){
+        leftPos = leftPos + width;
+        star.push(
+            <div className="star" key={`star-${index}`} style={{left: index * width, width: width, marginRight: spacing}}>
+            </div>
+        );
+    if(decimals > 0 && value <=5){
+        star.push(
+            <div className="star" key={`star-${index}`} style={{left: leftPos, width: width - spacing}}>
+            </div>
+        );
+    }
+}
+
+    const starPlaceHolder = [];
+    for(let index =0; index < 5; index++){
+        starPlaceHolder.push(
+            <div className="star placeholder" key={`starPlaceholder-${index}`} style={{left: index * width, width: width, marginRight: spacing}}>
+            </div>
+        );
+    }
+
+    return (
+        <>
+        <div className={["stars", className].join(" ")}>
+        {starPlaceHolder}
+        {star}
+        </div>
+        </>
+    );
+}
+
+Star.propTypes = {
+    className: propTypes.string,
+    value: propTypes.number,
+    width: propTypes.number,
+    height: propTypes.number,
+    spacing: propTypes.number 
+}
