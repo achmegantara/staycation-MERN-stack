955a0f07f8f33a3db94f8ec365a03fd146a397d9
achmegantara <achmegantara@gmail.com>
2021-03-04 22:56:15 +0700

    adding react-reveal and fade transition each section

--- front-end/src/parts/Hero.js
+++ front-end/src/parts/Hero.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Fade from "react-reveal/Fade";
 import ImageHero from "assets/images/img-hero.jpg";
 import ImageHero_ from "assets/images/img-hero-frame.jpg";
 import IconCities from "assets/images/icons/ic_cities.svg";
@@ -18,6 +18,7 @@ export default function Hero(props) {
     }
 
     return (
+        <Fade bottom>
         <section className="container pt-4">
             <div className="row align-items-center">
             <div className="col-auto pr-5" style={{width:530}}>
@@ -66,5 +67,6 @@ export default function Hero(props) {
                 </div>
             </div>
         </section>
-    )
+        </Fade>
+    );
 }
