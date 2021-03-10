2bee1597a8f8fcbe388b49b805d1b3320c9496ef
achmegantara <achmegantara@gmail.com>
2021-03-10 23:46:24 +0700

    finish details page

--- front-end/src/pages/DetailsPage.js
+++ front-end/src/pages/DetailsPage.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
+import Fade from "react-reveal/Fade";
 import Header from "parts/Header";
 import PageDetailTitle from "parts/PageDetailTitle";
 import FeaturedImage from "parts/FeaturedImage";
 import PageDetailDescription from "parts/PageDetailDescription";
 import BookingForm from "parts/BookingForm";
 import ItemDetails from "json/itemDetails.json";
+import Categories from "parts/Categories";
+import Testimonial from "parts/Testimony";
+import Footer from "parts/Footer";
 
 export default class DetailsPage extends Component {
 
@@ -30,13 +34,20 @@ render(){
     <section className="container">
         <div className="row">
             <div className="col-7 pr-5">
+                <Fade bottom>
                 <PageDetailDescription data={ItemDetails} />
+                </Fade>
             </div>
             <div className="col-5">
+            <Fade bottom>
             <BookingForm itemDetails={ItemDetails}></BookingForm>
+            </Fade>
             </div>
         </div>
     </section>
+    <Categories data={ItemDetails.categories}></Categories>
+    <Testimonial data={ItemDetails.testimonial}></Testimonial>
+    <Footer/>
     </>
 );
 }
