35d76cc04ac25a4207cd2883347234cc0353d8f3
achmegantara <achmegantara@gmail.com>
2021-04-02 21:00:29 +0700

    convert template sbadmin to app & install nodemon for automatic restart

--- back-end/server-staycation/app.js
+++ back-end/server-staycation/app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/sb-admin-2', express.static(path.join(__dirname, 'node_modules/startbootstrap-sb-admin-2')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
