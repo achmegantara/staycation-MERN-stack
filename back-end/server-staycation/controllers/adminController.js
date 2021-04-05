d4aab56dd1ea6e603a33e6aeeadd150b245ffd05
achmegantara <achmegantara@gmail.com>
2021-04-05 18:41:48 +0700

    setting view booking

--- back-end/server-staycation/controllers/adminController.js
+++ back-end/server-staycation/controllers/adminController.js
@@ -14,5 +14,9 @@ module.exports = {
 
     viewItem: (req, res) => {
         res.render('admin/item/view_item');
+    },
+
+    viewBooking: (req,res) => {
+        res.render('admin/booking/view_booking');
     }
 }
