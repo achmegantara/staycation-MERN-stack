13f9064883a1c95457a8eabe25aa678a9fd49fa1
achmegantara <achmegantara@gmail.com>
2021-04-14 14:26:03 +0700

    CRUD - show item data by id item

--- back-end/server-staycation/controllers/adminController.js
+++ back-end/server-staycation/controllers/adminController.js
@@ -217,7 +217,6 @@ module.exports = {
     try {
         const { id } = req.params;
         const item = await Item.findOne({_id: id}).populate({path: 'imageId', select: 'id imageUrl'});
-        console.log(item.imageId);
         const alertMessage = req.flash('alertMessage');
         const alertStatus = req.flash('alertStatus');
         const alert = {message : alertMessage, status: alertStatus};
@@ -233,6 +232,30 @@ module.exports = {
         res.redirect('/admin/item');
     }
     },
+
+    showEditItem: async (req, res) => {
+        try {
+            const { id } = req.params;
+            const item = await Item.findOne({_id: id})
+                .populate({path: 'imageId', select: 'id imageUrl'})
+                .populate({path: 'categoryId', select: 'id name'});
+            const category = await Category.find();
+            const alertMessage = req.flash('alertMessage');
+            const alertStatus = req.flash('alertStatus');
+            const alert = {message : alertMessage, status: alertStatus};
+            res.render('admin/item/view_item', {
+                title: "Staycation | Edit Item",
+                alert,
+                item,
+                category,
+                action: 'edit'
+            });
+        } catch (error) {
+            req.flash('alertMessage', `${error.message}`);
+            req.flash('alertStatus', 'danger');
+            res.redirect('/admin/item');
+        }
+        },
 
     viewBooking: (req,res) => {
         res.render('admin/booking/view_booking', {
