ccae4c2e22bbff1e696b3454322900a988502835
achmegantara <achmegantara@gmail.com>
2021-04-15 23:05:20 +0700

    CRUD on feature - update

--- back-end/server-staycation/controllers/adminController.js
+++ back-end/server-staycation/controllers/adminController.js
@@ -336,7 +336,7 @@ module.exports = {
                 alert,
                 itemId,
                 feature
-            });
+            })
         } catch (error) {
             req.flash('alertMessage', `${error.message}`);
             req.flash('alertStatus', 'danger');
@@ -369,6 +369,34 @@ module.exports = {
             req.flash('alertMessage', `${error.message}`);
             req.flash('alertStatus', 'danger');
             res.redirect(`/admin/item/show-detail-item/${itemId}`);
+        }
+    },
+    
+    editFeature: async(req,res) => {
+        const {id, name, qty, itemId} = req.body;
+        try {
+            const feature = await Feature.findOne({_id:id});
+            if(req.file == undefined){
+                feature.name = name;
+                feature.qty = qty;
+                await feature.save();
+                req.flash('alertMessage', 'Success Update feature');
+                req.flash('alertStatus', 'success');
+                res.redirect(`/admin/item/show-detail-item/${itemId}`);
+            } else {
+                await fs.unlink(path.join(`public/${feature.imageUrl}`));
+                feature.name = name;
+                feature.qty = qty;
+                feature.imageUrl = `images/${req.file.filename}`;
+                await feature.save();
+                req.flash('alertMessage', 'Success Update feature');
+                req.flash('alertStatus', 'success');
+                res.redirect(`/admin/item/show-detail-item/${itemId}`);
+            }
+        } catch (error) {
+            req.flash('alertMessage', `${error.message}`);
+            req.flash('alertStatus', 'danger');
+            es.redirect(`/admin/item/show-detail-item/${itemId}`);
         }
     },
 
