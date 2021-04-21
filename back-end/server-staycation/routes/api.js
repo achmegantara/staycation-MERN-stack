a069e0d5c3f5bb3dadf2040e627b308cab304081
Achmad Akbar Megantara <achmegantara@gmail.com>
2021-04-21 21:38:41 +0700

    testing api with postman

---
+++ back-end/server-staycation/routes/api.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-undef */
+const router = require('express').Router();
+const apiController = require('../controllers/apiController');
+// const { uploadSingle, uploadMultiple } = require('../middlewares/multer');
+
+router.get('/landing-page', apiController.landingPage);
+
+module.exports = router;
