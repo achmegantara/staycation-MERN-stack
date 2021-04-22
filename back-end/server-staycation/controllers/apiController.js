25642eae992f78ae7311ecdacf978258f1a42893
Achmad Akbar Megantara <achmegantara@gmail.com>
2021-04-22 16:39:20 +0700

    testing api landing page part 3

--- back-end/server-staycation/controllers/apiController.js
+++ back-end/server-staycation/controllers/apiController.js
@@ -1,4 +1,6 @@
 const Item = require('../models/Item');
+const Treasure = require('../models/Activity');
+const Traveler = require('../models/Booking');
 
 module.exports = {
     landingPage: async (req,res) => {
@@ -7,7 +9,19 @@ module.exports = {
             .select('_id title country city price unit imageId')
             .limit(5)
             .populate({path : 'imageId', select: '_id imageUrl'});
-            res.status(200).json({ mostPicked });
+
+            const traveler = await Traveler.find();
+            const treasure = await Treasure.find();
+            const city = await Item.find();
+
+            res.status(200).json({
+                hero: { 
+                    travelers : traveler.length,
+                    treasures : treasure.length,
+                    cities : city.length 
+                },
+                mostPicked
+            });
         } catch (error) {
             
         }
