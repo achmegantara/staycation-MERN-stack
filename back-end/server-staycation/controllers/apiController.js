f6f2e4b3376520c98dfc8cd5d55d04b608f789e1
Achmad Akbar Megantara <achmegantara@gmail.com>
2021-04-22 13:08:30 +0700

    testing api landing page part 2

--- back-end/server-staycation/controllers/apiController.js
+++ back-end/server-staycation/controllers/apiController.js
@@ -1,6 +1,15 @@
+const Item = require('../models/Item');
+
 module.exports = {
-    landingPage: (req,res) => {
-        const message = 'Hello Json';
-        res.status(200).json({message});
+    landingPage: async (req,res) => {
+        try {
+            const mostPicked = await Item.find()
+            .select('_id title country city price unit imageId')
+            .limit(5)
+            .populate({path : 'imageId', select: '_id imageUrl'});
+            res.status(200).json({ mostPicked });
+        } catch (error) {
+            
+        }
     }
 }
